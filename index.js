f7d6c14354af78b808bdf10c0a8b02e84b8f2ba6
Devanshu8412soni <110346808+Devanshu8412soni@users.noreply.github.com>
2023-08-29 10:10:14 +0530

    Summarize app

---
+++ index.js
@@ -0,0 +1,45 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+  });
+
+  app.post('/getresponse',async(req,res)=>{
+    try {
+        const {url}  = req.query;
+        const {data} = await axios.get(url);
+        // console.log(data);
+        const $ = cheerio.load(data);
+        const p = $("p").text()
+        res.json(p)
+        console.log(p);
+    } catch (error) {
+        console.log(error);
+    }
+  })
+  
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  });
+
+
+const fetchData = async ()=>{
+  try {
+    const {data} = await axios.get('https://www.wikipedia.org');
+    // console.log(data);
+    const $ = cheerio.load(data);
+    const p = $("h1").text()
+    return p;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+  
