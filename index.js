20bb903e8b473920afaf87396b0a35559c924172
Devanshu8412soni <110346808+Devanshu8412soni@users.noreply.github.com>
2023-08-30 17:17:32 +0530

    Chnages in index files

--- index.js
+++ index.js
@@ -6,39 +6,55 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-  });
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
 
-  app.post('/getresponse',async(req,res)=>{
-    try {
-        const {url}  = req.query;
-        const resp = await axios.get(url);
-        if(resp.status === 200)
-        {
-        const {data} = resp
-        console.log(data);
-        const $ = cheerio.load(data);
-        let p = $("p").text()
-       
-        console.log(p);
-        
-        p = p.substring(0,3000);      
-        res.json(p)
-        }
-        else{
-          res.status(400).json(resp)
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error)
+app.post("/getresponse", async (req, res) => {
+  try {
+    const { url } = req.query;
+    const resp = await axios.get(url);
+    if (resp.status === 200) {
+      const { data } = resp;
+      console.log(data);
+      const $ = cheerio.load(data);
+
+      //Extract text form <p> tags
+      const pTagsText = $("p")
+        .map((index, element) => $(element).text())
+        .get();
+
+      // Extract text from <h1> to <h6> tags
+      const hTagsText = $("h1, h2, h3, h4, h5, h6")
+        .map((index, element) => $(element).text())
+        .get();
+
+      // Extract text from <a> tags
+      const aTags = $("a")
+        .map((index, element) => $(element).text())
+        .get();
+
+      // Combine all the extracted text into a single array
+      const allText = [...pTagsText, ...hTagsText, ...aTags];
+
+      const resultedText = allText.join(' ');
+      const responseText = resultedText.substring(0,3000);
+
+      res.json({responseText});
+      // let p = $("p").text();
+
+      // console.log(p);
+
+      // p = p.substring(0, 3000);
+      // res.json(p);
     }
-  })
-  
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
-
-  
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
